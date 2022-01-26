4c8400c538f1c2d9fb050f1334271e949fbde989
ErgunAhmet <AhmetErgun2000@protonmail.com>
2022-01-26 18:14:15 +0100

    initial commit

---
+++ src/test/java/com/datajpa/relationship/RelationshipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.datajpa.relationship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelationshipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
