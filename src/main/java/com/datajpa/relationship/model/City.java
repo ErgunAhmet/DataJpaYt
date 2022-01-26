87a7e131c6d08003124b7963c01a10307a2915cf
ErgunAhmet <AhmetErgun2000@protonmail.com>
2022-01-26 18:27:58 +0100

    city entity

---
+++ src/main/java/com/datajpa/relationship/model/City.java
@@ -0,0 +1,21 @@
+package com.datajpa.relationship.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor
+@Data
+@Table(name = "City")
+public class City {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+
+    public City(String name) {
+        this.name = name;
+    }
+}
